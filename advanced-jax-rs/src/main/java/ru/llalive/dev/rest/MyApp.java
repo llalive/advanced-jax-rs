20894575b58cfcb94ec36cea37889ac9c395a422
Babikov_PV <babikov_pv@mirtv.ru>
2018-01-31 15:20:01 +0300

    Init commit.

---
+++ advanced-jax-rs/src/main/java/ru/llalive/dev/rest/MyApp.java
@@ -0,0 +1,9 @@
+package ru.llalive.dev.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("webapi")
+public class MyApp extends Application {
+	
+}
